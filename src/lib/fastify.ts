e33115303b19da8d983fbd04405856387174db97
Aimen-Allah <aimenftaiti@gmail.com>
2023-02-16 22:34:54 +0100

    feat: add error handler to support 400, 404 and 500

--- src/lib/fastify.ts
+++ src/lib/fastify.ts
@@ -1,6 +1,29 @@
-import fastify, { RouteOptions } from 'fastify'
+import fastify, { FastifyError, FastifyReply, FastifyRequest, RouteOptions } from 'fastify'
 import { userRoutes } from '../routes/users.routes'
-import { assertsSchemaBodyParamsQueryPresenceHook } from '../errors/MissingValidationElementsError'
+import { MissingValidationElementsError, assertsSchemaBodyParamsQueryPresenceHook } from '../errors/MissingValidationElementsError'
+import { ValidationError } from 'class-validator'
+import { EntityNotFoundError } from 'typeorm'
+
+const errorHandler = (
+    error: FastifyError,
+    request: FastifyRequest,
+    reply: FastifyReply
+) => {
+    if (process.env.NODE_ENV === 'production' && reply.statusCode >= 500) {
+        void reply.status(500).send({ error: 'Internal Server Error' })
+    } else if (reply.statusCode < 500) {
+        void reply.send(error)
+    } else {
+        void reply.status(500).send({ error: error.message })
+    }
+
+    if (error instanceof ValidationError)
+        void reply.status(400).send({ error: error.constraints })
+    if (error instanceof EntityNotFoundError)
+        void reply.status(404).send({ error: error.message })
+    if (error instanceof MissingValidationElementsError)
+        void reply.status(400).send({ error: error.message })
+}
 
 export const server = fastify({
     ajv: {
@@ -11,6 +34,7 @@ export const server = fastify({
 })
     //.addHook('onRoute', assertsResponseSchemaPresenceHook)
     .addHook('onRoute', assertsSchemaBodyParamsQueryPresenceHook)
+    .setErrorHandler(errorHandler)
     .register(userRoutes, { prefix: '/web-api/users' })
 
 export function assertsResponseSchemaPresenceHook(routeOptions: RouteOptions) {
@@ -18,3 +42,4 @@ export function assertsResponseSchemaPresenceHook(routeOptions: RouteOptions) {
         throw new Error('The response schema for the route is not present. Please provide it.')
     }
 }
+
